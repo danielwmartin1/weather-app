8344de368c7bd248c1400f3f31c51c009be7b507
danielwmartin1 <danielwmartin1@gmail.com>
2024-11-27 04:41:07 -0500

    Refactor CSS variables for padding and margin; enhance CurrentWeather component with capitalization for weather conditions

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -13,8 +13,17 @@ const CurrentWeather = ({ weatherData }) => {
     return directions[index];
   };
 
+  const capitalizeFirstLetter = (str) => {
+    return str
+      .split(' ')              // Split the string into an array of words
+      .map(word =>              // Apply the function to each word
+        word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()  // Capitalize the first letter, and make the rest lowercase
+      )
+      .join(' ');              // Join the array back into a single string
+  };
+
   const weatherDetails = [
-    { label: 'Conditions', value: weatherData.weather[0].description },
+    { label: 'Conditions', value: capitalizeFirstLetter(weatherData.weather[0].description) },
     { label: 'Current Temp', value: `${Math.round(weatherData.main.temp)}°${weatherData.units === 'metric' ? 'C' : 'F'}` },
     { label: 'Feels like', value: `${Math.round(weatherData.main.feels_like)}°${weatherData.units === 'metric' ? 'C' : 'F'}` },
     { label: 'High', value: `${Math.round(weatherData.main.temp_max)}°${weatherData.units === 'metric' ? 'C' : 'F'}` },
@@ -26,8 +35,6 @@ const CurrentWeather = ({ weatherData }) => {
     { label: 'Sunset', value: new Date(weatherData.sys.sunset * 1000).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' }) },
     { label: 'Visibility', value: `${weatherData.visibility / 1000} ${weatherData.units === 'metric' ? 'km' : 'mi'}` },
     { label: 'Pressure', value: `${weatherData.units === 'metric' ? weatherData.main.pressure + ' hPa' : (weatherData.main.pressure * 0.750062).toFixed(2) + ' mmHg'}` },
-    //{ label: 'Dew Point', value: `${Math.round(weatherData.main.temp - (weatherData.main.temp - ((100 - weatherData.main.humidity) / 5)))}°${weatherData.units === 'metric' ? 'C' : 'F'}` },
-    //{ label: 'UV Index', value: weatherData.uvi || 'N/A' },
     { label: 'Wind Direction', value: getWindDirection(weatherData.wind.deg) || 'N/A' },
     { label: 'Chance of Precipitation', value: `${weatherData.pop ? weatherData.pop * 100 : 0}%` },
     { label: 'Precipitation', value: `${weatherData.rain && weatherData.rain['1h'] ? weatherData.rain['1h'] : 0} ${weatherData.units === 'metric' ? 'mm' : 'in'}` },
@@ -35,19 +42,22 @@ const CurrentWeather = ({ weatherData }) => {
 
   return (
     <div className="current-weather">
-        <h2>{weatherData.name}, {weatherData.sys.country}</h2>
-        <h3 className='time'>
+      <div className="locationTimeContainer">
+        <h2 className="locationTime">{weatherData.name}, {weatherData.sys.country}</h2>
+        <h3 className='locationTime'>
           {time.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' })}
         </h3>
-    {weatherDetails.map((detail, index) => (
-      <p className='details' key={index}>{detail.label}: {detail.value}</p>
-    ))}
-
-    <img 
-      src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} 
-      alt={weatherData.weather[0].description} 
-    />
-  </div>
+      </div>
+      <div className="weather-detail">
+        {weatherDetails.map((detail, index) => (
+         <p className='details' key={index}>{detail.label}: {detail.value}</p>
+        ))}
+      </div>
+      <img 
+        src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} 
+        alt={weatherData.weather[0].description} 
+      />
+    </div>
   );
 };
 
