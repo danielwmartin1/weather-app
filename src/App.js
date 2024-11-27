d29f893347b8902dd23a766de9130f7b83283c53
danielwmartin1 <danielwmartin1@gmail.com>
2024-11-27 02:50:44 -0500

    Refactor App component to implement routing with React Router; adjust layout in App.css for improved spacing and responsiveness

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import './index.css';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 const App = () => {
   const [weatherData, setWeatherData] = useState(null);
@@ -21,27 +23,35 @@ const App = () => {
     setWeatherData(weather);
     setForecastData(forecast);
   };
-  
-
   return (
-    <div className="app-container">
-      <Header />
-      <div className="app">
-        <Search onSearch={handleSearch} />
-        {weatherData && forecastData ? (
-          <>
-            <CurrentWeather weatherData={weatherData} />
-              <hr />
-            <h2 className='fiveDay'>5-Day Forecast</h2>
-            <Forecast forecastData={forecastData} />
-          </>
-        ) : (
-          null
-        )}
+    <Router>
+      <div className="app-container">
+        <Header />
+        <div className="app">
+          <Routes>
+            <Route path="/" element={
+              <>
+                <Search onSearch={handleSearch} />
+                {weatherData && forecastData ? (
+                  <>
+                    <CurrentWeather weatherData={weatherData} />
+                    <hr />
+                    <h2 className='fiveDay'>5-Day Forecast</h2>
+                    <Forecast forecastData={forecastData} />
+                  </>
+                ) : (
+                  null
+                )}
+              </>
+            } />
+            {/* Add more routes here as needed */}
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 };
 
 export default App;
+
