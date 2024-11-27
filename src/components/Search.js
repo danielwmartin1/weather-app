5d8465154a65b5f9029b80f3575fc74733c79544
danielwmartin1 <danielwmartin1@gmail.com>
2024-11-27 05:39:23 -0500

    Add state abbreviation mapping to location formatting in Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -15,6 +15,67 @@ const Search = ({ onSearch }) => {
         onSearch(location);
         setLocation('');
     };
+    const stateLabelValues = [
+        { 'label':'Alabama', 'value': 'AL' },
+        { 'label':'Alaska', 'value': 'AK'},
+        { 'label':'American Samoa', 'value': 'AS'},
+        { 'label':'Arizona', 'value': 'AZ'},
+        { 'label':'Arkansas', 'value': 'AR'},
+        { 'label':'California', 'value': 'CA'},
+        { 'label':'Colorado', 'value': 'CO'},
+        { 'label':'Connecticut', 'value': 'CT'},
+        { 'label':'Delaware', 'value': 'DE'},
+        { 'label':'District of Columbia', 'value': 'DC'},
+        { 'label':'States of Micronesia', 'value': 'FM'},
+        { 'label':'Florida', 'value': 'FL'},
+        { 'label':'Georgia', 'value': 'GA'},
+        { 'label':'Guam', 'value': 'GU'},
+        { 'label':'Hawaii', 'value': 'HI'},
+        { 'label':'Idaho', 'value': 'ID'},
+        { 'label':'Illinois', 'value': 'IL'},
+        { 'label':'Indiana', 'value': 'IN'},
+        { 'label':'Iowa', 'value': 'IA'},
+        { 'label':'Kansas', 'value': 'KS'},
+        { 'label':'Kentucky', 'value': 'KY'},
+        { 'label':'Louisiana', 'value': 'LA'},
+        { 'label':'Maine', 'value': 'ME'},
+        { 'label':'Marshall Islands', 'value': 'MH'},
+        { 'label':'Maryland', 'value': 'MD'},
+        { 'label':'Massachusetts', 'value': 'MA'},
+        { 'label':'Michigan', 'value': 'MI'},
+        { 'label':'Minnesota', 'value': 'MN'},
+        { 'label':'Mississippi', 'value': 'MS'},
+        { 'label':'Missouri', 'value': 'MO'},
+        { 'label':'Montana', 'value': 'MT'},
+        { 'label':'Nebraska', 'value': 'NE'},
+        { 'label':'Nevada', 'value': 'NV'},
+        { 'label':'New Hampshire', 'value': 'NH'},
+        { 'label':'New Jersey', 'value': 'NJ'},
+        { 'label':'New Mexico', 'value': 'NM'},
+        { 'label':'New York', 'value': 'NY'},
+        { 'label':'North Carolina', 'value': 'NC'},
+        { 'label':'North Dakota', 'value': 'ND'},
+        { 'label':'Northern Mariana Islands', 'value': 'MP'},
+        { 'label':'Ohio', 'value': 'OH'},
+        { 'label':'Oklahoma', 'value': 'OK'},
+        { 'label':'Oregon', 'value': 'OR'},
+        { 'label':'Palau', 'value': 'PW'},
+        { 'label':'Pennsylvania', 'value': 'PA'},
+        { 'label':'Puerto Rico', 'value': 'PR'},
+        { 'label':'Rhode Island', 'value': 'RI'},
+        { 'label':'South Carolina', 'value': 'SC'},
+        { 'label':'South Dakota', 'value': 'SD'},
+        { 'label':'Tennessee', 'value': 'TN'},
+        { 'label':'Texas', 'value': 'TX'},
+        { 'label':'Utah', 'value': 'UT'},
+        { 'label':'Vermont', 'value': 'VT'},
+        { 'label':'Virgin Islands', 'value': 'VI'},
+        { 'label':'Virginia', 'value': 'VA'},
+        { 'label':'Washington', 'value': 'WA'},
+        { 'label':'West Virginia', 'value': 'WV'},
+        { 'label':'Wisconsin', 'value': 'WI'},
+        { 'label':'Wyoming', 'value': 'WY'}
+    ];
 
     useEffect(() => {
         const fetchLocationName = async (lat, lon) => {
@@ -24,10 +85,11 @@ const Search = ({ onSearch }) => {
             const data = await response.json();
             if (data && data.length > 0) {
                 const locationName = data[0].name;
-                const state = data[0].state ? data[0].state : '';
-                const county = data[0].county ? data[0].county : '';
+                const stateName = data[0].state;
                 const countryName = data[0].country;
-                setLocation(`${locationName}${county ? `, ${county}` : ''}${state ? `, ${state}` : ''}, ${countryName}`);
+                const state = stateLabelValues.find(state => state.label === stateName);
+                const stateAbbreviation = state ? state.value : stateName;
+                setLocation(`${locationName}${stateAbbreviation ? `, ${stateAbbreviation}` : ''}, ${countryName}`);
             }
         };
 
@@ -37,6 +99,7 @@ const Search = ({ onSearch }) => {
                 fetchLocationName(latitude, longitude);
             });
         }
+    // eslint-disable-next-line
     }, []);
 
     return (
